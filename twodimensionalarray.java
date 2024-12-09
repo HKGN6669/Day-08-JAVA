c7899a1ca95d88804821b51d85a0d02f2f822d5f
HKGN6669 <tayyibafathima666@gmail.com>
2024-12-09 15:03:16 +0530

    Add files via upload

---
+++ twodimensionalarray.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class twodimensionalarray{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //initializaton of 2D array
+        int a[][]={{1,2},{3,4}};
+        //accessing 2D array
+        System.out.println(Arrays.toString(a[0]));
+        System.out.println(Arrays.toString(a[1]));
+    }
+}
